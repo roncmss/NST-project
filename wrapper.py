78aa20630e07184d3b3041a9f8b1794dc8c3ca50
Ron Luo <ronluo951031@gmail.com>
2022-04-16 15:08:25 -0700

    added wrapper

---
+++ wrapper.py
@@ -0,0 +1,15 @@
+
+import os
+
+cameraCommand = "python3 neural_style_transfer_video.py --models models"
+
+if __name__ == "__main__":
+    print("************ Welcome to our Neural Style Transfer Demonstration ************")
+    print("Apply to image: 1\nApply to camera feed: 2")
+    userChoice = int(input("Make your choice: "))
+    if userChoice == 1:
+        print("image")
+    elif userChoice == 2:
+        os.system(cameraCommand)
+    else:
+        raise ValueError("you suck!")
